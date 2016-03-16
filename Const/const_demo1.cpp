79d4939c9a538eeccad1d28157eb2d08890dba13
penghui <penghuilater@gmail.com>
2016-03-17 00:16:10 +0800

    add const demo

--- Const/const_demo1.cpp
+++ Const/const_demo1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(void) {
-
     const int a = 0;
-    const int a = 10;
-    cout << "a = " << a;
+    cout << "a = " << a << endl;
+    
+    int age = 39;
+    const int* pt = &age; //a pointer to const int
+    cout << "&age " << &age << endl; 
+    cout << "pt:" << *pt << " " << pt << endl;
+    
+    int tomato = 40;
+    pt = &tomato;
+    cout << "tomato:" << pt << endl;
+
+    int potato = 45;
+    int* const finger = &potato; //a const pointer to int
+    cout << "&potato: " << &potato << endl;
+    cout << "finger:" << finger << endl;
+    cout << "&finger:" << &finger << endl;
+
 }

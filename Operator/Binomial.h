8a7bd8932d9da7c2f180a7fa8f3c6b065c12bc1a
penghui <penghuilater@gmail.com>
2016-03-20 14:04:43 +0800

    add friend operator function demo

--- Operator/Binomial.h
+++ Operator/Binomial.h
@@ -1,11 +1,13 @@
 class Binomial {
-
+        friend Binomial operator-(Binomial &bin);
     public:
         Binomial(int a, int b);
         ~Binomial();
         int getA();
         int getB();
-        Binomial operator-();
+        //Binomial operator-();
+        Binomial operator++();
+        Binomial operator++(int);// 该int值表示是操作符的重载是做后置++的重载
     private:
         int m_a;
         int m_b;
@@ -22,10 +24,26 @@ int Binomial::getA() {
 int Binomial::getB() {
     return m_b;
 }
-Binomial Binomial::operator-() {
-    this->m_a = -(this->m_a);
-    m_b = -m_b;
+//Binomial Binomial::operator-() {
+//    this->m_a = -(this->m_a);
+//    m_b = -m_b;
+//    return *this;
+//}
+
+Binomial operator-(Binomial &bin) {
+    bin.m_a = -bin.m_a;
+    bin.m_b = -bin.m_b;
+    return bin;
+}
+
+Binomial Binomial::operator++() {
+    m_a++;
+    m_b++;
     return *this;
 }
 
-
+Binomial Binomial::operator++(int) {
+    Binomial old(*this);
+    m_a++;
+    m_b++;
+}

2fe3bc85141e3e6c6b6933eb3fe868ff092fef09
pangpangpang3 <penghui@linuxdeepin.com>
2016-07-19 15:42:30 +0800

    Add template demo6

---
+++ Template/template_demo6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+T sum(T a, T b) {
+    return (a+b);
+};
+
+int main() {
+    int a = 1, b = 2;
+    double c = 10.0, d = 20.0;    
+    cout << sum(a, b) << endl;
+    cout << sum(c, d) << endl;
+   // cout << sum("a", "b") << endl;
+    return 0;
+
+}
